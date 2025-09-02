3acde663bb2ac68f128a4d4d30df2e9297f0d64f
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-02 10:16:16 +0000

    edit

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def lowercaseAlphabets():
-
-    # lowercase
-    for c in range(97, 123):
-        print(chr(c), end = " ");
-
-    print("");
+for c in range(97, 123):
+        print(chr(c), end = " ")

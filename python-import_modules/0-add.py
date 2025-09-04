f7c5611c00f4e3e43bb13d331dbc52c30978214e
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-04 03:35:25 +0000

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import add_0 as add
+    import add_0 as addition
 
-a = 1
-b = 2
-c = add.add(a, b)
+    a = 1
+    b = 2
+    c = addition.add(a, b)
     print("{} + {} = {}".format(a, b, c))

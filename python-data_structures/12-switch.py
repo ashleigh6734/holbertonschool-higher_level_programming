04d1e20bc72dbd4ea6180f870bfb2094580aa7a7
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-08 17:18:29 +1000

    removing comments

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -3,9 +3,3 @@ a = 89
 b = 10
 a, b = b, a
 print("a={:d} - b={:d}".format(a, b))
-# a, b = b, a is the Pythonic way to swap values
-# The following is a more traditional way to swap values
-# temp = a
-# a = b
-# b = temp
-# print("a={:d} - b={:d}".format(a, b))

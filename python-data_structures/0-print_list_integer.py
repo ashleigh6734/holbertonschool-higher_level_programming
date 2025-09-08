1b1e66df6b0d086b9c43ca04114e5eb795107109
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-08 16:45:12 +1000

    task0

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+def print_list_integer(my_list=[]):
+    """Print all integers of a list.
+
+    Args:
+        my_list (list): List of integers.
+    """
+    for integer in my_list:
+        print("{:d}".format(integer))

098b20a69a3c76361bf699650bebf053b4a4c736
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-16 15:02:05 +1000

    rm whitespace

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -24,7 +24,7 @@ class Square:
           nothing
         """
         if not isinstance(size, int):
-            raise TypeError ("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size

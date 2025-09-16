5d9ca6139b1f68584eea0ff024c392aa4aa521d1
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-16 14:02:24 +1000

    adding comments

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,6 +3,12 @@
 class Square:
     pass
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+"""
+Just and empty class
+Args:
+    None
+Returns:
+    None
+    Raises:
+        Nothing
+"""

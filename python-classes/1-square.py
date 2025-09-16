30f28335f8da488ae95eae4d0b8a4ff0a3cbfc6d
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-16 14:13:30 +1000

    task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module for class "Square"
+"""
+
+class Square:
+    """
+    class that defines a square by: (based on 0-square.py)
+    Args:
+      size (int): size of a side of the square
+
+    Returns:
+      nothing
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the square with a given size
+        Args:
+          size (int): size of a side of the square
+
+        Returns:
+          nothing
+        """
+        self.__size = size

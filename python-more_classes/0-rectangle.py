d6db7b60cf781ddd2571ea3e329d50e56d133091
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-19 22:31:41 +1000

    adding docstring

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-Module for class "Square"
+Module for class "Rectangle"
 NB: class is empty for task 0
 """
 
 
 class Rectangle:
+    """Defines an empty Rectangle class."""
     pass
     """
     empty class

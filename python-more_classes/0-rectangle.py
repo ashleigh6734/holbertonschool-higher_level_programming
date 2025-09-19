e918cb11ee91d118b2a34df7357651441690e296
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-19 22:29:09 +1000

    task0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module for class "Rectangle"
+Class is empty for task 0
+"""
+
+
+class Rectangle:
+    pass
+    """
+    empty class
+    Args:
+      nothing
+
+    Returns:
+      nothing
+    """

0cef5fd4bb3aaa0594045ef86340189245bf3a8d
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-20 11:05:16 +1000

    pycodestyle

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
-        else: 
+        else:
             return 2 * (self.width + self.height)
 
     """getters"""

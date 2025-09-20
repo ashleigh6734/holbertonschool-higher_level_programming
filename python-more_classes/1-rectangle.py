7b36619ee98eb73e799a2ed3f6a2a53b7abaefc6
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-20 10:48:08 +1000

    task1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""
+Module for class "Rectangle"
+"""
+
+
+class Rectangle:
+    """
+    class that defines a Rectangle by: (based on 0-Rectangle.py)
+    Args:
+      width (int): width of a side of the Rectangle
+
+    Returns:
+      nothing
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes the Rectangle with a given width
+        Args:
+          width (int): width of a side of the Rectangle
+
+        Returns:
+          nothing
+        """
+        self.__width = width
+        self.__height = height
+
+    """getter"""
+    @property
+    def width(self):
+        """ Retrieves the width of the Rectangle
+        """
+        return self.__width
+
+    """setter"""
+    @width.setter
+    def width(self, value):
+        """ Sets the width of the Rectangle
+        Args:
+          value (int): width of a side of the Rectangle
+
+        Returns:
+          nothing
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    """getter"""
+    @property
+    def height(self):
+        """ Retrieves the width of the Rectangle
+        """
+        return self.__height
+
+    """setter"""
+    @height.setter
+    def height(self, value):
+        """ Sets the width of the Rectangle
+        Args:
+          value (int): width of a side of the Rectangle
+
+        Returns:
+          nothing
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = value

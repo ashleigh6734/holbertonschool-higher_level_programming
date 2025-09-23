611ed45fa3471124dbabc3fb77b8719ba0329fec
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-23 09:45:13 +0000

    adding type

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ Write a function that returns True if the object is an instance of a class
 
 def inherits_from(obj, a_class):
     """Return True if obj is an instance of a_class, else False."""
-    return issubclass(obj, a_class)
+    return issubclass(type(obj), a_class)

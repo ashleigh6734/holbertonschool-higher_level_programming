7107ee44470fb6e1132056e9e50fed7642f41016
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-23 09:39:04 +0000

    task3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,5 +5,5 @@ Write a function that returns True if the object is an instance of
 
 
 def is_same_class(obj, a_class):
-    """Return True if obj is exactly an instance of a_class, else False."""
+    """Return True if obj is an instance of a_class, else False."""
     return isinstance(obj, a_class)

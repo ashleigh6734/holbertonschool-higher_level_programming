fdb2706d6739ad5db9eb30b53954b37494a3995c
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-23 09:03:37 +0000

    task1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Write a class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """ Class generated, which inherits from list """
+    def print_sorted(self):
+        print(sorted(self))

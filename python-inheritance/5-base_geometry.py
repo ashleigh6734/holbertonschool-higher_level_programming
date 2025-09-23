d7caf1ba40ae7a48297c8b64ddd70e8b36346db8
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-23 09:48:08 +0000

    task5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module for class "BaseGeometry"
+NB: class is empty for task 0
+"""
+
+
+class BaseGeometry:
+    pass
+    """
+    empty class
+    Args:
+      nothing
+
+    Returns:
+      nothing
+    """

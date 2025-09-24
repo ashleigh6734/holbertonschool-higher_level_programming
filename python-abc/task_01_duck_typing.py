1a954229a39e63213b36c230ce11d4bb750c26a0
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-24 21:54:28 +1000

    task1

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Creating an abstract class
+"""
+
+
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    """Abstract shape class."""
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return math.pi * self.radius ** 2
+    
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+    
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+    
+def shape_info(self):
+    print(f"Area: {Shape.area()}")
+    print(f"Perimeter: {Shape.perimeter()}")

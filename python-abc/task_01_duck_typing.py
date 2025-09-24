907f886b5acc38d705ac73edc5d8acf4115b14da
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-24 21:58:12 +1000

    edit

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -20,8 +20,8 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
-        if radius <= 0:
-            raise ValueError("radius must be greater than zero")
+        if radius < 0:
+            radius = radius * -1
         self.radius = radius
     
     def area(self):

6715238e12f3766753237050c60979a19c6c46ca
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-25 10:28:33 +0000

    edit

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,17 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-class CountedIterator(iter):
-    def __init__(self, iterable):
-        self.iterator = iter(iterable)
-        self.count = 0
+class CountedIterator():
+
+    def __init__(self, list):
+        self.my_iterator = iter(list)
+        self.counter = 0
 
     def __next__(self):
-        item = next(self.iterator)
-        self.count += 1
-        return item
+        try:
+            result = self.my_iterator.__next__()
+        except StopIteration:
+            raise StopIteration
+        self.counter += 1
+        return result
 
     def get_count(self):
-        return self.count
-
-    def __iter__(self):
-        return self
+        return self.counter

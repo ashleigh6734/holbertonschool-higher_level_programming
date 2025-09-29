5295de3d2ab9b57a459e41d74e9b9224b3039994
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-29 22:50:45 +1000

    PEP8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,7 +2,6 @@
 """Function that returns the dictionary description for JSON"""
 
 
-
 def class_to_json(obj):
     """Returns the dictionary description for JSON serialization"""
     return obj.__dict__

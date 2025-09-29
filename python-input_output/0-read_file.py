074f14977b92745b2f4167d92944e8100eabd640
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-29 21:12:03 +1000

    adding loop

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout."""
-    with open("my_file_0.txt", "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read())

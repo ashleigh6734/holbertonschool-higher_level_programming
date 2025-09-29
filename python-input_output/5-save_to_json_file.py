792b439f067fd99147b8c93885c17bbcaf0c60b2
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-29 22:25:58 +1000

    pycodestyle

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Function that writes an Object to a text file, using a JSON representation"""
+"""Function that writes an Object to a text file, using JSON"""
 
 
 import json
+
 
 def save_to_json_file(my_obj, filename):
     """Writes an Object to a text file, using a JSON representation"""

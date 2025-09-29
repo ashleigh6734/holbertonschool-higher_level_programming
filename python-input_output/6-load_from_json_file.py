983d71cb069263dcae3bcf130cc75aa50f350ca3
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-29 22:35:54 +1000

    adding "return"

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """Write a function that creates an Object from a JSON file"""
-    with open(filename, "w", encoding="utf-8") as f:
-        json.load(f)
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)

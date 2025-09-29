85b1d444f815135c3720e39c0462870e9dd8ef87
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-29 21:14:43 +1000

    fix: modify print statement to avoid extra newline in read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         for lines in f:
-            print(lines)
+            print(lines, end="")

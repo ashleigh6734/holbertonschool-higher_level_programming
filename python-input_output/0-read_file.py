c4ca7a98a6951991c2398b22e15fce99c7a686e3
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-29 21:13:35 +1000

    refactor read_file function to print lines individually

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        for lines in f:
+            print(lines)

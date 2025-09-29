7003e8c9ef412737ac2a6bd9959abc70030d04c9
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-29 22:42:48 +1000

    pycodestyle

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a script that adds all arguments to a Python list, and then save them to a file"""
+"""Write a script that adds all arguments to a Python list"""
 
 import sys
 import os

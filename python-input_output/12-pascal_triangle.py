ed89018c85126b37b9871f3662fec9fb0fdf44d9
ashleigh6734 <ashleigh6734@gmail.com>
2025-09-30 13:48:52 +1000

    edit

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""Returns a list of lists of integers representing the Pascal’s triangle of n"""
+
 
 def pascal_triangle(n):
     """ Returns a list of lists of integers representing the Pascal's triangle of n
     """
+
     if n <= 0:
         return []
 
-    triangle = []
+    triangle = [] # First row
 
     for row in range(n):
         new_row = [1] * (row + 1)

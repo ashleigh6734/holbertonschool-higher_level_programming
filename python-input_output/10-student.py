e57eb7dfb5e8faf1ddff16c6adc8f7a389ce9df8
ashleigh6734 <ashleigh6734@gmail.com>
2025-10-01 03:33:07 +0000

    task10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Defining the student class
+"""
+
+
+class Student:
+    """ Student Class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initiliase class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Return dictionary representation
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            a_dict = {}
+            for i in attrs:
+                try:
+                    a_dict[i] = getattr(self, i)
+                except AttributeError as e:
+                    pass
+        return a_dict

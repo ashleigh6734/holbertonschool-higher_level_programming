7645559214e57f036bfacc11f1a1ddde7b1895c5
ashleigh6734 <ashleigh6734@gmail.com>
2025-10-01 03:06:26 +0000

    edit

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Write a class Student that defines a student """
+""" Defining the student class
+"""
 
 
-class student:
-    """Student class that defines a student """
+class Student:
+    """ Student Class
+    """
+
     def __init__(self, first_name, last_name, age):
+        """ Initiliase class
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """ Return dictionary representation of a Student instance
+        """ Return dictionary representation
         """
         return self.__dict__

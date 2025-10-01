52586e472608dd233d140655d6dcadc1bbc53b7f
ashleigh6734 <ashleigh6734@gmail.com>
2025-10-01 12:00:47 +0000

    edit 1

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -9,4 +9,25 @@ import pickle
 
 class CustomObject:
     """Defining the CustomObject class"""
-    
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Prints the object's attributes in a formatted style."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(csl, filename):
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception:
+            return None

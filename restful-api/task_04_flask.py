3fc1b44734ab0e96f7ed4ea9313d780fd244f7af
ashleigh6734 <ashleigh6734@gmail.com>
2025-10-09 21:03:43 +1100

    push

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask API!"
+
+
+if __name__ == "__main__": app.run()

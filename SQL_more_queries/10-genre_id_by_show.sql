a59579ed5cc5cd31cf931a2b8f1c8db655315bba
ashleigh6734 <ashleigh6734@gmail.com>
2025-10-18 16:04:48 +1100

    TASK10 EDIT

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
 WHERE tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.id ASC;
+ORDER BY tv_shows.id ASC, tv_show_genres.genre_id ASC;

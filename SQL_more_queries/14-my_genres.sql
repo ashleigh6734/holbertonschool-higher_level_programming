ab12aa26d83baf3e471c7936a155ab8660838991
ashleigh6734 <ashleigh6734@gmail.com>
2025-10-20 20:28:08 +1100

    edit

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -5,5 +5,5 @@ JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = "Comedy"
+WHERE tv_shows.title = "Dexter"
 ORDER BY tv_genres.name ASC;

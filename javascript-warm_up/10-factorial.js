2b21ae9ac6dd373367dbcbdcaa7bf6c5b857ffca
ashleigh6734 <ashleigh6734@gmail.com>
2025-11-02 19:49:03 +1100

    task10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,18 @@
 #!/usr/bin/node
 
-function factorial(n) {
-  if (n <= 1) return 1;
-  return n * factorial(n - 1);
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+
+function factorial (a) {
+  if (a <= 1) {
+    return (1);
+  } else {
+    return (a * factorial(a - 1));
+  }
 }
 
-const input = parseInt(process.argv[2]);
-console.log(factorial(Number.isNaN(input) ? 1 : input));
+if (isNaN(num)) {
+  console.log('1');
+} else {
+  console.log(factorial(num));
+}
